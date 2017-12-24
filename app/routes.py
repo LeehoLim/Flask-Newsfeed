4967f59725ee834f8e5e49803d554c18d8f0b538
LeehoLim <leeholim@uchciago.edu>
2017-12-24 02:38:48 -0800

    Adding error handlers

--- app/routes.py
+++ app/routes.py
@@ -6,27 +6,6 @@ from app.models import User
 from datetime import datetime
 from app.forms import EditProfile
 
-@app.route('/edit_profile', methods=['GET','POST'])
-@login_required
-def edit_profile():
-	form = EditProfile()
-	if form.validate_on_submit():
-		current_user.username = form.username.data
-		current_user.about_me = form.about_me.data
-		db.session.commit()
-		flash('Settings have been updated.')
-		return redirect(url_for('edit_profile'))
-	elif request.method == 'GET':
-		form.username.data = current_user.username
-		form.about_me.data = current_user.about_me
-	return render_template('edit_profile.html', title='Settings', form=form)
-
-@app.before_request
-def before_request():
-	if current_user.is_authenticated:
-		current_user.last_seen = datetime.utcnow()
-		db.session.commit()
-
 
 
 @app.route('/')
@@ -99,4 +78,30 @@ def user(username):
 def logout(): 
 	logout_user() #Built into flask_login, logout function
 	return redirect(url_for('index')) #Redirects to index after
+
+#Edit Profile
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm(current_user.username)
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.about_me = form.about_me.data
+		db.session.commit()
+		flash('Settings have been updated.')
+		return redirect(url_for('edit_profile'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.about_me.data = current_user.about_me
+	return render_template('edit_profile.html', title='Settings', form=form)
+
+#Get the time for the last time the user was seen
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
+
+
+
 
